484dbfd9d7405c26aa6dbbc8568bdb885abd0a34
D1sl <94911312+D1sl@users.noreply.github.com>
2022-01-04 11:25:52 -0600

    Pseudocoding the app

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 // Assignment code here
 
+// Available characters (ADD REST ONCE CODE WORKS)
+upperCaseCharacter = ["A", "B", "C", "D", "E", "F"]; 
+lowerCaseCharacter = ["a", "b", "c", "d", "e", "f"];
+numberCharacter = ["1", "2", "3", "4", "5", "6"];
+specialCharacter = ["!", "@", "#", "€", "%", "&", "/", "(", ")", "=", "?"];
+
+// 1. App goes through a series of questions to determine what kind of a password the user wants
+
+// 1.1. Password length
+// 1.2. Ask if user wants uppercase letters
+// 1.3. Ask if user wants lowercase letters
+// 1.4. Ask if user wants special characters
+
+// 2. App goes through some kind of loop that concatenates a random letter from each array into a draft string and repeats it as long as the amount of requested characters are met 
+
+// 3. App reorganizes the draft string into the final password
+
+
+
+
+
+
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
