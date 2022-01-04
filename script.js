33bcff94222319d751c9cdda8d940bc01e86ca3c
D1sl <94911312+D1sl@users.noreply.github.com>
2022-01-04 11:37:02 -0600

    Added arrays, variables to store user answers and console.log to make sure answers are stored

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 // Assignment code here
 
 // Available characters (ADD REST ONCE CODE WORKS)
-upperCaseCharacter = ["A", "B", "C", "D", "E", "F"]; 
+upperCaseCharacter = ["A", "B", "C", "D", "E", "F"];
 lowerCaseCharacter = ["a", "b", "c", "d", "e", "f"];
 numberCharacter = ["1", "2", "3", "4", "5", "6"];
 specialCharacter = ["!", "@", "#", "€", "%", "&", "/", "(", ")", "=", "?"];
 
+// Store users answers in variables
+var askPasswordLength = "";
+var askUpperCaseCharacters = false;
+var askLowerCaseCharacters = false;
+var askNumberCharacters = false;
+var askSpecialCharacters = false;
+
+
 // 1. App goes through a series of questions to determine what kind of a password the user wants
+
+function generatePassword() {
+  var askPasswordLength = window.prompt("How long would you like your password to be? Answer with a number between 8-128.");
+  var askUpperCaseCharacters = window.confirm("Would you like your password to include uppercase characters?")
+  var askLowerCaseCharacters = window.confirm("Would you like your password to include uppercase characters?")
+  var askNumberCharacters = window.confirm("Would you like your password to include lowercase characters?")
+  var askSpecialCharacters = window.confirm("Would you like your password to include special characters?")
+
+  console.log("Password length: " + askPasswordLength);
+  console.log("Uppercase characters: " + askUpperCaseCharacters);
+  console.log("Lowercase characters: " + askLowerCaseCharacters);
+  console.log("Number characters: " + askNumberCharacters);
+  console.log("Special characters: " + askSpecialCharacters);
+
+}
+
+
 
 // 1.1. Password length
 // 1.2. Ask if user wants uppercase letters
